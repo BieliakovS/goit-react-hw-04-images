66d30a3fba84d9f24877d7bee6e2616bfada89b0
BieliakovS <93442308+BieliakovS@users.noreply.github.com>
2023-05-25 01:01:37 +0300

    2

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -3,35 +3,39 @@ import PropTypes from 'prop-types';
 import css from './Searchbar.module.css';
 
 const Searchbar = ({ onSubmit }) => {
-  const [query, setQuery] = useState('');
+const [query, setQuery] = useState('');
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    onSubmit(query);
-  };
+const handleSubmit = e => {
+e.preventDefault();
+onSubmit(query);
+};
 
-  const handleChange = e => {
-    setQuery(e.target.value);
-  };
+const handleChange = e => {
+setQuery(e.target.value);
+};
 
-  return (
-    <header className={css.Searchbar}>
-      <form className={css.SearchForm} onSubmit={handleSubmit}>
-        <button type="submit" className={css.SearchFormButton}>
-          <span className={css.SearchFormButtonLabel}>Search</span>
-        </button>
-        <input
-          className={css.SearchFormInput}
-          type="text"
-          autoComplete="off"
-          autoFocus
-          placeholder="Search images and photos"
-          value={query}
-          onChange={handleChange}
-        />
-      </form>
-    </header>
-  );
+return (
+<header className={css.Searchbar}>
+<form className={css.SearchForm} onSubmit={handleSubmit}>
+<button type="submit" className={css.SearchFormButton}>
+<span className={css.SearchFormButtonLabel}>Search</span>
+</button>
+<input
+       className={css.SearchFormInput}
+       type="text"
+       autoComplete="off"
+       autoFocus
+       placeholder="Search images and photos"
+       value={query}
+       onChange={handleChange}
+     />
+</form>
+</header>
+);
+};
+
+Searchbar.propTypes = {
+onSubmit: PropTypes.func.isRequired,
 };
 
 export default Searchbar;
